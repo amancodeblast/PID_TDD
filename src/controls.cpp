339c22cf57ae3c975a87f90ff2cc92c31aee51aa
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-10-03 21:18:49 -0400

    Updated controls.cpp
    Fixed getTime() method of Controls class to rightly return dt_

--- src/controls.cpp
+++ src/controls.cpp
@@ -2,6 +2,7 @@
  * @file Controls.hpp
  * @author Aman Sharma (amankrsharma3@gmail.com)
  * Driver: Aman Sharma Navigator: Sharmitha Ganesan
+ * @author Programmer: Tanuj Thakkar (tanuj@umd.edu)
  * @brief Controls Calss declaration
  * @version 0.1
  * @date 2022-10-01
@@ -9,23 +10,26 @@
  * @copyright Copyright (c) 2022
  *
  */
+
 #include "controls.hpp"
+
+
 Controls::Controls(double Kp, double Kd, double Ki, double dt) {
   Kp_ = Kp;
   Kd_ = Kd;
   Ki_ = Ki;
   dt_ = dt;
 }
+
 /**
  * @brief get inputs
  *
- * @param dt_
  * @return int
  */
 double Controls::getTime() {
-  dt_ = -1;
   return dt_;
 }
+
 /**
  * @brief Computes the final velocity
  *
