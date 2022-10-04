794301580d4913bb4dcf01b5220ffc5cdedf20fa
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-10-04 12:38:46 -0400

    Updated main.cpp
    Added activity to take user input for PID gain parameters, target and actual velocity and compute PID achieved velocity.

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,7 @@
  * @file main.cpp
  * @author Aman Sharma (amankrsharma3@gmail.com)
  * Driver: Aman Sharma Navigator: Sharmitha Ganesan
+ * @author Programmer: Tanuj Thakkar (tanuj@umd.edu)
  * @brief Main function for the PID Controls Classs
  * @version 0.1
  * @date 2022-10-01
@@ -9,7 +10,24 @@
  * @copyright Copyright (c) 2022
  *
  */
-#include <iostream>
 
+#include <iostream>
 #include "controls.hpp"
-int main() { return 0; }
+
+int main() {
+  double Kp, Ki, Kd, dt;
+  double target, actual;
+
+  std::cout << "Enter the gain parameters of the PID controller: Kp, Ki, Kd, dt"
+            << std::endl;
+  cin >> Kp >> Ki >> Kd >> dt;
+
+  std::cout << "Enter the target and actual velocities: " << std::endl;
+  std::cin >> target >> actual;
+
+  Controls c(Kp, Kd, Ki, dt);
+  std::cout << "PID achieved velocity: " << c.computeVel(target, actual)
+            << std::endl;
+
+  return 0;
+}
